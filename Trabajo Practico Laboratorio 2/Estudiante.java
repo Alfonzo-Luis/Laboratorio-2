726d348163f3354578505df9a93d6cdf23199ade
Alfonzo-Luis <89166702+Alfonzo-Luis@users.noreply.github.com>
2021-09-29 20:39:48 -0300

    Add files via upload
    Subida Trabajo Practico

---
+++ Trabajo Practico Laboratorio 2/Estudiante.java
@@ -0,0 +1,19 @@
+package com.company;
+
+class Estudiante extends Persona{
+    public Estudiante(String nombre, String apellido, int legajo){
+        super.setNombre(nombre);
+        super.setApellido(apellido);
+        super.setLegajo(legajo);
+    }
+
+    @Override
+    public void modificarDatos() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante= " + super.toString();
+    }
+}
